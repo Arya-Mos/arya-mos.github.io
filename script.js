07a7bd536623002d4b92978c707a530b8e92d95c
AryaMos <kingmoose191@gmail.com>
2020-11-21 08:38:55 +0000

    background color and text alignment

---
+++ script.js
@@ -0,0 +1,10 @@
+* {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+script {
+    display: none;
+}
+body {
+    text-align: center;
+    background-color: rgba(206 147, 255, 0.781);
+}
